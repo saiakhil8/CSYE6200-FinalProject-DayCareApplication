d1530599b6084bd9ea97477bda3c2e65905aaf63
Sai Akhil <saiakhil90@gmail.com>
2021-12-14 09:53:45 -0500

    Add FunctionalUtilities.java

---
+++ src/main/java/edu/neu/csye6200/Utils/FunctionalUtilities.java
@@ -0,0 +1,16 @@
+package edu.neu.csye6200.Utils;
+
+/**
+ * @author SaiAkhil
+ */
+public class FunctionalUtilities {
+    @FunctionalInterface
+    public interface BiFunction<One, Two> {
+        void accept(One one, Two two);
+    }
+
+    @FunctionalInterface
+    public interface TriFunction<One, Two, Three> {
+        void accept(One one, Two two, Three three);
+    }
+}
