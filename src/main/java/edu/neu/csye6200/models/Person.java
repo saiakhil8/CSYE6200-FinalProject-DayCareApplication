d879dcd27f1302ba7f380a05ffb0554aa18ef3fe
Sai Akhil <saiakhil90@gmail.com>
2021-12-14 09:53:06 -0500

    Add Person

---
+++ src/main/java/edu/neu/csye6200/models/Person.java
@@ -0,0 +1,14 @@
+package edu.neu.csye6200.models;
+
+/**
+ * @author SaiAkhil
+ */
+public abstract class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String password;
+    private String dateOfBirth;
+
+}
