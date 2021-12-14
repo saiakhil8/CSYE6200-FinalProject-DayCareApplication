07f7d1e3a5e7e735db42644589bb01484d252595
Sai Akhil <saiakhil90@gmail.com>
2021-12-14 09:53:32 -0500

    Update Constants.java

--- src/main/java/edu/neu/csye6200/Utils/Constants.java
+++ src/main/java/edu/neu/csye6200/Utils/Constants.java
@@ -1,5 +1,8 @@
 package edu.neu.csye6200.Utils;
 
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
 /**
  * @author SaiAkhil
  */
@@ -10,10 +13,15 @@ public class Constants {
     public static int APP_PREFERRED_WIDTH = 1024;
     public static int APP_PREFERRED_HEIGHT = 2048;
 
-    public static int APP_DEFAULT_BACKGROUND_TYPE=0;
+    public static int APP_DEFAULT_BACKGROUND_TYPE = 0;
     public static String APP_BACKGROUND = "#0391F6";
 
     public static final int EVENT_NEXT_SCREEN = 7001;
     public static final int EVENT_LOGOUT = 7099;
+
+    public static Function<String, Boolean> VALIDATE_EMAIL_ADDRESS = (emailId) -> {
+        Pattern pattern = Pattern.compile("^.+@.+\\..+$");
+        return pattern.matcher(emailId).matches();
+    };
 
 }
