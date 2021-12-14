878d27ffc37261677deae5ee19895ded590c4ad5
Sai Akhil <saiakhil90@gmail.com>
2021-12-14 09:53:21 -0500

    Add AdminDashboardController and Layout

---
+++ src/main/java/edu/neu/csye6200/controllers/AdminDashboardController.java
@@ -0,0 +1,20 @@
+package edu.neu.csye6200.controllers;
+
+import edu.neu.csye6200.Listeners;
+import edu.neu.csye6200.views.ApplicationLayout;
+
+/**
+ * @author SaiAkhil
+ */
+public class AdminDashboardController extends AppViewsController {
+
+    @Override
+    public ApplicationLayout getAppPage() {
+        return null;
+    }
+
+    @Override
+    protected void goToNextScreen(Listeners.AppControlEventListener appControlListener) {
+
+    }
+}
---
+++ src/main/java/edu/neu/csye6200/views/AdminDashboardLayout.java
@@ -0,0 +1,23 @@
+package edu.neu.csye6200.views;
+
+import java.awt.*;
+
+/**
+ * @author SaiAkhil
+ */
+public class AdminDashboardLayout extends ApplicationLayout {
+
+    public AdminDashboardLayout(String imagePathOrColor, int backgroundType) {
+        super(imagePathOrColor, backgroundType);
+    }
+
+    @Override
+    protected Component getMainLayoutComponent() {
+        return null;
+    }
+
+    @Override
+    protected void initComponents() {
+
+    }
+}
