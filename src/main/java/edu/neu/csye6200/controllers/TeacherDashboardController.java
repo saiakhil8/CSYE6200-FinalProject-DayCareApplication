c3c2501cf7b1676fbd6fe770ddedeb5db5651076
Sai Akhil <saiakhil90@gmail.com>
2021-12-15 21:25:34 -0500

    Add TeacherDashboard and its Layout

---
+++ src/main/java/edu/neu/csye6200/controllers/TeacherDashboardController.java
@@ -0,0 +1,19 @@
+package edu.neu.csye6200.controllers;
+
+import edu.neu.csye6200.Listeners;
+import edu.neu.csye6200.views.ApplicationLayout;
+
+/**
+ * @author SaiAkhil
+ */
+public class TeacherDashboardController extends AppViewsController {
+    @Override
+    public ApplicationLayout getAppPage() {
+        return null;
+    }
+
+    @Override
+    protected void goToNextScreen(Listeners.AppControlEventListener appControlListener) {
+
+    }
+}
